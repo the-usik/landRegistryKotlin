be9321df920a0ed2979820adc5bdc819f927e6a8
the-usik <me@usik.space>
2020-11-23 17:15:43 +0300

    changed events

--- src/main/kotlin/events/events.kt
+++ src/main/kotlin/events/events.kt
@@ -2,9 +2,5 @@ package events
 
 import tornadofx.FXEvent
 
-object GeneralPageEvent : FXEvent()
-object AddPageEvent : FXEvent()
 object EditPageEvent : FXEvent()
-object RemoveEvent : FXEvent()
-object SettingsPageEvent : FXEvent()
-object AboutPageEvent : FXEvent()
+object DeleteConfirmationEvent : FXEvent()
